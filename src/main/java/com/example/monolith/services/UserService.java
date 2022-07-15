98f4db2752b9199cf30a18bb3f3824c9e0794825
Teodor Maeli <teodor.maeli@brainstarstech.com>
2022-07-16 01:03:05 +0300

    UserService interface

---
+++ src/main/java/com/example/monolith/services/UserService.java
@@ -0,0 +1,9 @@
+package com.example.monolith.services;
+
+import com.example.monolith.utility.exceptions.ObjectAlreadyExistException;
+
+public interface UserService<T,T1,T2> {
+    String createUser(T type) throws ObjectAlreadyExistException;
+    String createStudentAccount(T1 type) throws ObjectAlreadyExistException;
+    String createTeacherAccount(T2 type) throws ObjectAlreadyExistException;
+}
