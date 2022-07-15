195c5c9fa37568c288076483fd59c2d74e80488e
Teodor Maeli <teodor.maeli@brainstarstech.com>
2022-07-15 09:14:11 +0300

    some eddit

--- src/main/java/com/example/monolith/securityConfig/SecurityConfig.java
+++ src/main/java/com/example/monolith/securityConfig/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
